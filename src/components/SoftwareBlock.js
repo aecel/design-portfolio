3c4c549f5c244445284d2d106c849e44b63463bf
aecel <aecelacuna@gmail.com>
2023-03-16 18:41:48 +0800

    Edit SoftwareBlock

--- src/components/SoftwareBlock.js
+++ src/components/SoftwareBlock.js
@@ -1,5 +1,16 @@
-const SoftwareBlock = () => {
-  return <div></div>
+const SoftwareBlock = ({ icon, text }) => {
+  return (
+    <div className="rounded-square">
+      {icon ? (
+        <div className="rounded-square-icon-container">
+          <img className="rounded-square-icon" src={icon} alt="" />
+        </div>
+      ) : (
+        <></>
+      )}
+      <div className="rounded-square-text">{text}</div>
+    </div>
+  )
 }
 
 export default SoftwareBlock
