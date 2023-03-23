ff9cb08f6f2ff172dffa156cbe1d8cc0aa8247fa
aecel <aecelacuna@gmail.com>
2023-03-23 17:36:39 +0800

    Update SoftwareIUse to be scrollable

--- src/components/SoftwareIUse.js
+++ src/components/SoftwareIUse.js
@@ -1,5 +1,30 @@
-const SoftwareIUse = () => {
-  return <section className="main-section"></section>
+import { useEffect, useRef } from "react"
+import SpacingForHeader from "./SpacingForHeader"
+
+const SoftwareIUse = ({ triggerRef, triggerRef2 }) => {
+  const sectionRef = useRef()
+
+  const scroll = () => {
+    sectionRef.current.scrollIntoView({ behavior: "smooth", block: "start" })
+  }
+
+  useEffect(() => {
+    const trigger = triggerRef.current
+    trigger.addEventListener("click", scroll)
+    const trigger2 = triggerRef2.current
+    trigger2.addEventListener("click", scroll)
+
+    return () => {
+      trigger.removeEventListener("click", scroll)
+      trigger2.removeEventListener("click", scroll)
+    }
+  }, [triggerRef, triggerRef2])
+
+  return (
+    <section ref={sectionRef} className="main-section">
+      <SpacingForHeader />
+    </section>
+  )
 }
 
 export default SoftwareIUse
